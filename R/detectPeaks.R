3b3d93a3c2e7af5b2dc5d8bd6a41c8fc80678edb
thomas-enzlein <t.enzlein@hs-mannheim.de>
2024-02-06 13:44:22 +0100

    - .detectPeaks got halfWindowSize argument. - halfWindowSize is now per default 3.

--- R/detectPeaks.R
+++ R/detectPeaks.R
@@ -3,16 +3,20 @@
 #' @param spec    MALDIquant::MassSpectrum or list thereof
 #' @param SNR     Numeric, signal to noise value
 #' @param method  Character, method see MALDIquant::detectPeaks
+#' @param halfWindowSize Numeric, defines width of window for peak detection. See `MALDIquant::detectPeaks()`.
 #'
 #' @details
 #' Just a wrapper around MALDIquant::detectPeaks to ensure that the returned peak list is named.
 #'
 #' @return
 #' List of MALDIquant::MassPeaks with the same names as `spec`
-.detectPeaks <- function(spec, SNR, method = "SuperSmoother") {
+.detectPeaks <- function(spec, SNR, method = "SuperSmoother", halfWindowSize = 3) {
   stopifnot(isMassSpectrum(spec)| isMassSpectrumList(spec))
 
-  peaks <- detectPeaks(spec, SNR = SNR, method = "SuperSmoother")
+  peaks <- detectPeaks(spec,
+                       SNR = SNR,
+                       method = "SuperSmoother",
+                       halfWindowSize = halfWindowSize)
   names(peaks) <- names(spec)
 
   return(peaks)
