dd1a4d2337a63800ebea63300d8fe29b9d8d394d
thomas-enzlein <t.enzlein@hs-mannheim.de>
2024-08-09 05:29:18 +0200

    fix error with micro symbol leading to errors in latex

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' MALDI mass spectra from SAHA treated cells
 #'
 #' Cells were treated with the following concentrations of SAHA:
-#' 0, 0.04, 0.12, 0.37, 1.11, 3.33, 10, 30 µM
+#' 0, 0.04, 0.12, 0.37, 1.11, 3.33, 10, 30 uM
 #' For each concentration there are 4 replicates ("measurement replicates").
 #'
 #' @format List of MALDIquant::MassSpectrum
