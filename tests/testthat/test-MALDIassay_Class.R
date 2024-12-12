7c15dd3b6165228ce1254a75c7b159acab24aaa2
thomas-enzlein <t.enzlein@hs-mannheim.de>
2024-12-12 16:30:26 +0100

    another fix for tests

--- tests/testthat/test-MALDIassay_Class.R
+++ tests/testthat/test-MALDIassay_Class.R
@@ -151,7 +151,7 @@ test_that("MALDIassy_class handles invalid inputs gracefully", {
                    normFactors = prc$normFac, mzShifts = prc$mzShift,
                    fits = res_list, stats = stat_df, included_specIdx = prc$idx,
                    settings = list(normMeth = "method")),
-               "avgSpectra must be a list of class MALDIquant::MassSpectrum objects.")
+               "avgSpectra must be a list of class MALDIquant::MassSpectrum or MALDIquant::MassPeaks objects.")
   
   # Test invalid avgPeaks
   expect_error(new("MALDIassay",
